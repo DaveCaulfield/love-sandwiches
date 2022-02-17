4c85b6fa5352c37ca51ea7de020213d00079fa24
DaveCaulfield <dave.caulfield@gmail.com>
2022-02-17 10:15:11 +0000

    Adjust validation check to insure all values provided are integers

--- run.py
+++ run.py
@@ -21,15 +21,20 @@ def get_sales_data():
     """
     Get sales figures imput from the user
     """
-  
-    print('Please enter sales data from the last market')
-    print('Data shuold be six numbers, seperated by commas')
-    print('Example: 10,20,30,40,50,60,\n')
+    while True:  
+        print('Please enter sales data from the last market')
+        print('Data shuold be six numbers, seperated by commas')
+        print('Example: 10,20,30,40,50,60,\n')
 
-    data_str = input('Enter your data here: ')
+        data_str = input('Enter your data here: ')
     
-    sales_data = data_str.split(",")
-    vailidate_data(sales_data)
+        sales_data = data_str.split(",")
+
+        if vailidate_data(sales_data):
+            print('Data is valid!')
+            break
+
+    return sales_data
 
 
 def vailidate_data(values):
@@ -47,6 +52,9 @@ def vailidate_data(values):
             )
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
+        return False
+
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
